b94a4bbf3ba3752345891ecc1179f54ea3573661
Michelle <kojsmn@miamioh.edu>
2015-11-14 22:56:13 +0000

    User.java implemented

--- WEB-INF/classes/kojsmn/models/User.java
+++ WEB-INF/classes/kojsmn/models/User.java
@@ -16,15 +16,57 @@ import java.io.IOException;
 public class User{
     public String username = "";
     public String password = "";
+    public int id = -1;
+    public boolean admin = false;
+
+    Database db = null;
 
     public User(){
+        db = new Database();
     }
 
     public boolean verifyUser(String username, String password){
+        try {
+            String sql = "SELECT id FROM User WHERE (username=? and password = sha2(?, 512))";
+            PreparedStatement stmt = db.conn.prepareStatement(sql);
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+        
+            if (rs.next()){
+                id = rs.getInt(1);
+                return true;
+            }
+            else{
+                return false;
+            }
+        } catch(Exception err){
+            System.err.println("Error on verifing User " + err.toString());
+            return false;
+        } 
         return false;
     }
 
     public boolean admin(String username, String password){
+        String sql;
+        PreparedStatement stmt;
+        ResultSet rs;
+
+        try {
+            sql = "SELECT Admin FROM User WHERE username=?";
+            stmt = db.conn.prepareStatement(sql);
+            stmt.setString(1, username);
+            rs = stmt.executeQuery();
+            
+            if (rs.next()){
+                admin = rs.getBoolean(1);
+                if (admin == true)
+                    return true;
+            }
+        } catch (Exception err){
+            System.err.println(err);
+        }
+        
         return false;
     }
 
@@ -32,13 +74,55 @@ public class User{
     }
 
     public void addUser(String username, String password){
+        String sql;
+        PreparedStatement stmt;
+        ResultSet rs;
+        
+        // See if username exists
+        try {
+            sql = "SELECT id FROM User WHERE username=?";
+            stmt = db.conn.prepareStatement(sql);
+            stmt.setString(1, username);
+            rs = stmt.executeQuery();
+            if (rs.next()){
+                System.out.println("Username already exists");
+            }
+        } catch (Exception err){
+            System.err.println("Error checking for username");
+        }
+
+        // Insert into table
+        try {
+            sql = "INSERT INTO User (username, password) VALUES (?, sha2(?, 512))";
+            stmt = db.conn.prepareStatement(sql);
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            stmt.executeUpdate();
+        }   catch (Exception err){
+            System.err.println("Error creating user");
+        }
+
+        // Get id for new user
+        try {
+            sql = "SELECT id FROM User WHERE username=?";
+            stmt = db.conn.prepareStatement(sql);
+            stmt.setString(1, username);
+            rs = stmt.executeQuery();
+            if (rs.next()){
+                id = rs.getInt(1);
+            } else {
+                System.out.println("Could not get id for new user");
+            }
+        } catch (Exception err){
+            System.err.println("Error getting id for new user");
+        }
     }
 
     public void deleteUser(String username, String password){
     }
 
     public String toString(){
-        return "toString";
+        return "Username: " + username + " id: " + id;
     }
 
      public static void main(String a[]) throws Exception {
@@ -46,9 +130,4 @@ public class User{
         System.out.println(u.toString());
 
     }
-
-    
-
-
-
 }
